bb55c85b068d1870c32cb5ee5280dc9b2f5b5675
selcuklu <yilmax4@gmail.com>
2022-09-14 13:58:20 +0300

    auth

--- users/views.py
+++ users/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def register(request):
-    return render(request, 'users/register.html')
+    form = UserCreationForm(request.POST or None)
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password2")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+
+        return redirect("home")
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)
+
 def login(request):
-    return render(request, 'users/login.html')
+
+    return redirect('home')
 def logout(request):
     return render(request, 'users/logout.html')
