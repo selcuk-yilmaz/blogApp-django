e76945f9b311771dc1afb322e4de526b8726990e
selcuklu <yilmax4@gmail.com>
2022-09-18 15:39:05 +0300

    go on

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import register,login
+from .views import register
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', register, name='register' ),
-   
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name="registration/password_change.html"), name="password_change")
 ]
